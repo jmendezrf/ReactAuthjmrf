6a917b74df003bb353a5fce5c701ec615dfa6cb1
Juan Francisco Méndez Rivera <jmendez@sar.gob.hn>
2019-07-25 14:04:34 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Table } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        DEVSESSIONS USERS CONTROL
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Table className="container" responsive striped bordered hover dark size='sm'>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td><Button className="btn-success">UPDATE</Button>{' '}</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            <td><Button className="btn-success">UPDATE</Button>{' '}</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            <td><Button className="btn-success">UPDATE</Button>{' '}</td>
+          </tr>
+          
+        </tbody>
+      </Table>
+      </header> 
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const myfirstelement = <h1>Hello React!</h1>
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
